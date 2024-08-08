ab59f9968d5a7fb927a031e9dc351f505a43a73f
bwill619 <bjw0619@gmail.com>
2024-08-08 02:56:44 -0400

    framework

--- src/App.js
+++ src/App.js
@@ -6,16 +6,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="title">
+          Brendon Williams
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <a className='subHeading'>
+          Charlotte, NC
         </a>
       </header>
     </div>
