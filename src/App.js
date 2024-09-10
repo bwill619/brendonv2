f89ca663558d9e3e50af99cf105fa664c622ee75
bwill619 <bjw0619@gmail.com>
2024-09-10 11:22:56 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p className="title">
-          Brendon Williams
+        <p className="title">Brendon Williams Charlotte, NC</p>
+        <a className="subHeading"></a>
+
+        <p className="Experience">Experience</p>
+
+        <p classnName="Projects">Projects</p>
+
+        <p className="Skills">Skills</p>
+
+        <p className="Contact Title">Contact</p>
+        <p className="Contact">
+        bwill619@icloud.com
+        https://www.linkedin.com/in/brendon-williams/
         </p>
-        <a className='subHeading'>
-          Charlotte, NC
-        </a>
+
       </header>
     </div>
   );
